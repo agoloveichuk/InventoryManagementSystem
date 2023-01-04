1e8807bf501836b734d1d35451f1991cff7d0bd8
agoloveichuk@gmail.com <agoloveichuk@gmail.com>
2023-01-04 17:53:19 +0200

    Add project files.

---
+++ IMS.UseCases/IInventoryRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace IMS.UseCases
+{
+    public interface IInventoryRepository
+    {
+    }
+}
---
+++ IMS.UseCases/ViewInventoryByName.cs
@@ -0,0 +1,18 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoryByName : IInventoryRepository
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoryByName(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async IEnumerable<Inventory> ExecuteAsync(string name)
+        {
+
+        }
+    }
+}
